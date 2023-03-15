40b16deb6a7da6986617d58cdd85da329f4dc21d
Daniel Suárez <dwsuarez98@gmail.com>
2023-03-14 21:16:04 -0300

    First commit, scrap sitio 1

---
+++ scrap.py
@@ -0,0 +1,65 @@
+#############################
+from bs4 import BeautifulSoup 
+import requests
+#############################
+
+# Constantes
+NOTEBOOKS_ML = 'https://listado.mercadolibre.com.ar/notebooks-usadas#D[A:notebooks%20usadas]'
+
+
+class ScrapMercadoLibre:
+
+    def validator_soup(self, url: str):
+        """ 
+        Paseador de HTML 
+        Args:
+            url : strin de url Mercado Libre
+        Returns:
+            soup: html parsed
+        """
+
+        header = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                }
+        page = requests.get(url, headers = header)
+
+        try:
+            soup = BeautifulSoup(page.content,"html.parser")
+        except:
+            return None
+        return soup
+
+    def notebooks_usadas(self):
+        """ 
+        Extrae los datos de las notebooks usadas
+        Returns:
+            Nombre
+            Precio
+        """
+        soup = self.validator_soup(NOTEBOOKS_ML)
+        nombres = soup.find_all('h2', class_= 'ui-search-item__title shops__item-title')
+        precios = soup.find_all('span', class_='price-tag-fraction')
+
+        if len(nombres) == 0 or len(precios) == 0:
+            return []
+
+        notebooks = []
+        for nombre, precio in zip(nombres, precios):
+            notebook = {
+                    'nombre': nombre.text.strip(),
+                    'precio': precio.text.strip()
+                    }
+            notebooks.append(notebook)
+        print(notebooks)
+
+        return notebooks
+
+        
+
+
+if __name__ == '__main__':
+    s1 = ScrapMercadoLibre()
+    s1.notebooks_usadas()
+
+
+
