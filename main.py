e8aa0a0db4c39283157e71d6ef21bf5ccd74eefb
Daniel Suárez <dwsuarez98@gmail.com>
2023-03-21 22:54:49 -0300

    Menú funcionando

--- main.py
+++ main.py
@@ -2,31 +2,54 @@ from scrap.scrap import ScrapMercadoLibre
 from exploratory_analysis.data_work import DataWork
 from database.database import DataBase
 
-scrap = ScrapMercadoLibre()
-data_work = DataWork()
-#db = DataBase()
+class Menu:
+    def __init__(self):
+        self.scrap = ScrapMercadoLibre()
+        self.data_work = DataWork()
+        self.db = DataBase()
 
+    def run(self):
+        while True:
+            print("Menú")
+            print("1 Extraer datos (Notebooks nuevas vs Notebooks usadas)")
+            print("2 Ejecutar Análisis de datos")
+            print("3 Mostrar análisis (Precio, Promedio, Percentiles)")
+            print("4 Registrar los datos en la base de datos")
+            print("5 Mostrar registros de la base de datos")
+            print("6 Mostar últimos tres registros")
+            print("7 Mostrar primeros registros")
+            print("9 Exit")
 
+            opcion = input("Seleciones una opción: ")
+
+            if opcion == "1":
+                self.scrap.save_data_csv()
+
+            elif opcion == "2":
+                self.data_work.main()
+
+            elif opcion == "3":
+                self.data_work.imprimir_analisis()
+
+            elif opcion == "4":
+                self.db.load_data()
+
+            elif opcion == "5":
+                self.db.show_data()
+
+            elif opcion == "6":
+                self.db.show_data_top()
+
+            elif opcion == "7":
+                self.db.show_data_first()
+
+            elif opcion == "9":
+                print("Programa finalizado")
+                break
+
+            else:
+                print("Opción incorrecta")
 
 if __name__ == '__main__':
-    while True:
-        print("Menú")
-        print("1 Extraer datos (Notebooks nuevas vs Notebooks usadas)")
-        print("2 Ejecutar Análisis de datos")
-        print("3 Mostrar análisis (Precio, Promedio, Percentiles)")
-        print("4 Extraer datos (Notebooks nuevas vs Notebooks usadas)")
-        print("5 Extraer datos (Notebooks nuevas vs Notebooks usadas)")
-        print("6 Extraer datos (Notebooks nuevas vs Notebooks usadas)")
-        print("6 Extraer datos (Notebooks nuevas vs Notebooks usadas)")
-
-        opcion = input("Seleciones una opción: ")
-
-        if opcion == "1":
-            scrap.save_data_csv()
-    
-        if opcion == "2":
-            data_work.main()
-    
-
-        if opcion == "3":
-            data_work.imprimir_analisis()
+    menu = Menu()
+    menu.run()
